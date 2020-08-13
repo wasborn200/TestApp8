6711e96d0cb896d70ac92caf14a7c80094236c0d
wasborn200 <archibald.ff1@gmail.com>
2020-08-13 17:49:39 +0900

    web setting

--- TestApp8/Controllers/HomeController.cs
+++ TestApp8/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestApp8.Models;
 
 namespace TestApp8.Controllers
 {
@@ -10,6 +13,27 @@ namespace TestApp8.Controllers
     {
         public ActionResult Index()
         {
+            List<HomeModel> homelist = new List<HomeModel>();
+            using (var conn = new SqlConnection(
+            ConfigurationManager.ConnectionStrings["DB"].ConnectionString))
+            {
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM test";
+                conn.Open();
+                using (var sdr = cmd.ExecuteReader())
+                {
+                    if (sdr.HasRows)
+                    {
+                        while (sdr.Read())
+                        {
+                            var id = sdr["ID"];
+                            var name = sdr["NAME"].ToString();
+                            ViewData["id"] = id;
+                            ViewData["name"] = name;
+                        }
+                    }
+                }
+            }
             return View();
         }
 
