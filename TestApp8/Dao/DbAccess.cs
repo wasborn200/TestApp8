3c1293ead46302f5fb7c5f1d7055db8e3b08e8c7
wasborn200 <archibald.ff1@gmail.com>
2020-08-19 08:02:41 +0900

    DbAccess.csを追加

---
+++ TestApp8/Dao/DbAccess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TestApp8.Dao
+{
+    public class DbAccess
+    {
+        public SqlConnection sqlCon { get; set; }
+        public DbAccess()
+        {
+            try
+            {
+                string sConnection = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+                this.sqlCon = new SqlConnection(sConnection);
+                this.sqlCon.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void close()
+        {
+            this.sqlCon.Close();
+            this.sqlCon.Dispose();
+        }
+
+        public DataTable executeQuery(SqlCommand cmd)
+        {
+            try
+            {
+                SqlDataReader read = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(read);
+                return dt;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
